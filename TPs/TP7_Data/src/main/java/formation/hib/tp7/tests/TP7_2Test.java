19f714666a8ac52820c3f0299865147815e9b2ec
David THIBAU <david.thibau@gmail.com>
2020-12-03 09:27:24 +0100

    Point de départ TP7

--- TPs/TP7_Data/src/main/java/formation/hib/tp7/tests/TP7_2Test.java
+++ TPs/TP7_Data/src/main/java/formation/hib/tp7/tests/TP7_2Test.java
@@ -13,29 +13,120 @@ import formation.hib.tp7.metier.Employe;
 
 public class TP7_2Test extends TestCase {
 	
-	// Récupérer tous les noms des employés et des départements en une seule requéte
-	public void testRecupererDepartementEtEmployes() throws Exception{
+// Récupérer tous les noms des employés et des départements en une seule requéte
+	public void testLazy() throws Exception {
 		System.out.println("\nCU : Consulter les départements et leurs employés");
-		System.out
-				.println("1 - L'utilisateur demande à charger les départements et leurs employés");
+		System.out.println("1 - L'utilisateur demande à charger les départements et leurs employés");
 		EntityManager em = DBHelper.getFactory().createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 
-		// A COMPLETER 
-		
-		List<Departement> departements =  hqlQuery.getResultList();
-		assertNotNull(departements);
+		// A compléter 
+
 		tx.commit();
 		em.close();
+
 		System.out.println("2 - Le système affiche tous les départements et leurs employés");
 		for (Departement d : departements) {
-			System.out.println("[Departement :" + d.getNom()+"]");
-			for ( Employe e : d.getEmployes() ) {
-				System.out.println("\t[Employé :" + e.getNom()+"]");
+			System.out.println("[Departement :" + d.getNom() + "]");
+			try {
+				for (Employe e : d.getEmployes()) {
+					System.out.println("\t[Employé :" + e.getNom() + "]");
+				}
+			} catch (LazyInitializationException e) {
+				System.out.println("LAZY EXCEPTION");
 			}
 		}
 	}
+
+	public void testAvoidLazyWithInitialize() throws Exception {
+		System.out.println("\nCU : Consulter les départements et leurs employés");
+		System.out.println("1 - L'utilisateur demande à charger les départements et leurs employés");
+		EntityManager em = DBHelper.getFactory().createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		// A compléter
+
+		assertNotNull(departements);
+		tx.commit();
+		em.close();
+
+		System.out.println("2 - Le système affiche tous les départements et leurs employés");
+		for (Departement d : departements) {
+			System.out.println("[Departement :" + d.getNom() + "]");
+			for (Employe e : d.getEmployes()) {
+				System.out.println("\t[Employé :" + e.getNom() + "]");
+			}
+		}
+	}
+
+	public void testAvoidLazyWithHSQL() throws Exception {
+		System.out.println("\nCU : Consulter les départements et leurs employés");
+		System.out.println("1 - L'utilisateur demande à charger les départements et leurs employés");
+		EntityManager em = DBHelper.getFactory().createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		// A compléter
+
+		tx.commit();
+		em.close();
+
+		System.out.println("2 - Le système affiche tous les départements et leurs employés");
+		for (Departement d : departements) {
+			System.out.println("[Departement :" + d.getNom() + "]");
+			for (Employe e : d.getEmployes()) {
+				System.out.println("\t[Employé :" + e.getNom() + "]");
+			}
+		}
+	}
+
+	public void testAvoidLazyWithFetchProfile() throws Exception {
+		System.out.println("\nCU : Consulter les départements et leurs employés");
+		System.out.println("1 - L'utilisateur demande à charger les départements et leurs employés");
+		EntityManager em = DBHelper.getFactory().createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+// A compléter
+
+		assertNotNull(departements);
+		tx.commit();
+		em.close();
+
+		System.out.println("2 - Le système affiche tous les départements et leurs employés");
+		for (Departement d : departements) {
+			System.out.println("[Departement :" + d.getNom() + "]");
+			for (Employe e : d.getEmployes()) {
+				System.out.println("\t[Employé :" + e.getNom() + "]");
+			}
+		}
+	}
+
+	
+	public void testAvoidLazyWithEntityGraph() throws Exception {
+		System.out.println("\nCU : Consulter les départements et leurs employés");
+		System.out.println("1 - L'utilisateur demande à charger les départements et leurs employés");
+		EntityManager em = DBHelper.getFactory().createEntityManager();
+				
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		// A compléter
+
+		assertNotNull(departements);
+		tx.commit();
+		em.close();
+
+		System.out.println("2 - Le système affiche tous les départements et leurs employés");
+		for (Departement d : departements) {
+			System.out.println("[Departement :" + d.getNom() + "]");
+			for (Employe e : d.getEmployes()) {
+				System.out.println("\t[Employé :" + e.getNom() + "]");
+			}
+		}
+	}
+
 	
 
 	

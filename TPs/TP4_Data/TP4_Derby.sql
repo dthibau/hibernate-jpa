5fc5ae1bcc2cbbd799ca231691a119988996fbd7
David THIBAU <david.thibau@gmail.com>
2021-02-16 14:54:50 +0100

    Suprression column idtype

--- TPs/TP4_Data/TP4_Derby.sql
+++ TPs/TP4_Data/TP4_Derby.sql
@@ -39,7 +39,6 @@ INSERT INTO tmission (libelle,debut,fin) VALUES('Forfait ILO','2004-04-01','2005
 DROP TABLE tformation;
 CREATE TABLE tformation (
   cours varchar(50) default '0',
-  IDType int default NULL,
   ID bigint default NULL
 ) ;
 

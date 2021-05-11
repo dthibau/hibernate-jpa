d91214dd71d77e21c2bec916ee79ddd1d7c674be
David THIBAU <david.thibau@gmail.com>
2021-05-11 09:41:29 +0200

    Correction TP4

--- TPs/TP4_Data/TP4_Postgres.sql
+++ TPs/TP4_Data/TP4_Postgres.sql
@@ -1,6 +1,12 @@
-
-
+DROP TABLE IF EXISTS temploye;
 DROP TABLE IF EXISTS tdpt;
+DROP TABLE IF EXISTS tforfait;
+DROP TABLE IF EXISTS tregie;
+DROP TABLE IF EXISTS tformation;
+DROP TABLE IF EXISTS tmission;
+
+
+
 CREATE TABLE tdpt (
   ID BIGSERIAL primary key ,
   nom varchar(30) default NULL,
@@ -12,7 +18,7 @@ INSERT INTO tdpt (nom,code) VALUES('E-Technologies','etch');
 INSERT INTO tdpt (nom,code) VALUES('Services tertiaires','ster');
 INSERT INTO tdpt (nom,code) VALUES('Administration','adm');
 
-DROP TABLE IF EXISTS temploye;
+
 CREATE TABLE temploye (
   Id BIGSERIAL primary key ,
   nom varchar(40) NOT NULL default '',
@@ -30,7 +36,6 @@ INSERT INTO temploye (nom,email,tel,entree,genre,iddpt ) VALUES('Jean Dupond','j
 INSERT INTO temploye (nom,email,tel,entree,genre,iddpt ) VALUES('Marianne Predaut','mp@plb.fr','01.41.21.61.32','2015-09-01','F','1');
 
 
-DROP TABLE IF EXISTS tmission;
 CREATE TABLE tmission (
   ID BIGSERIAL primary key,
   libelle varchar(200) default NULL,
@@ -42,17 +47,15 @@ INSERT INTO tmission (libelle,debut,fin) VALUES('Formation CASA','2004-06-21','2
 INSERT INTO tmission (libelle,debut,fin) VALUES('Audit Mthode projet TRANS','2004-07-01','2004-10-15');
 INSERT INTO tmission (libelle,debut,fin) VALUES('Forfait ILO','2004-04-01','2005-01-15');
 
-DROP TABLE IF EXISTS tformation;
 CREATE TABLE tformation (
   cours varchar(50) default '0',
   ID bigint default NULL
 ) ;
 
-INSERT INTO tformation VALUES('Modélisation Objet','2');
+INSERT INTO tformation VALUES('Modélisation Objet','1');
 
 
 
-DROP TABLE IF EXISTS tregie;
 CREATE TABLE tregie (
   taux int default '0',
   ID bigint default NULL
@@ -61,7 +64,6 @@ CREATE TABLE tregie (
 INSERT INTO tregie VALUES('7','2');
 
 
-DROP TABLE IF EXISTS tforfait;
 CREATE TABLE tforfait (
   IDFORFAIT bigint default '0',
   prix int default NULL,
